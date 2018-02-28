925740d427895a9e5563aba6791ea49c1d635302
Katelin DeSerano <katelindeserano@Katelins-MacBook-Pro.local>
2018-02-27 21:11:10 -0800

    added put endpoint

--- positive-me/router.js
+++ positive-me/router.js
@@ -33,10 +33,39 @@ router.post('/', jsonParser, (req, res) => {
     });
 });
 
-// Never expose all your journals like below in a prod application
-// we're just doing this so we have a quick way to see
-// if we're creating users. keep in mind, you can also
-// verify this in the Mongo shell.
+router.put('/:id', jsonParser, (req, res) => {
+    const requiredFields = ['negativeThought', 'negativeFeeling','negativeEvidence', 
+        'alternativeEvidence', 'positiveThought', 'positiveFeeling', 'date'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+    if (req.params.id !== req.body.id) {
+      const message = (
+        `Request path id (${req.params.id}) and request body id `
+        `(${req.body.id}) must match`);
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    Journal.findByIdAndUpdate(req.params.id, {$set:{
+      id: req.params.id,
+      negativeThought: req.body.negativeThought,
+      negativeFeeling: req.body.negativeFeeling,
+      negativeEvidence: req.body.negativeEvidence,
+      alternativeEvidence: req.body.alternativeEvidence,
+      positiveThought: req.body.positiveThought,
+      positiveFeeling: req.body.positiveFeeling,
+      date: req.body.date
+    }}, {new:true})
+    .exec()
+    .then(updated => res.status(200).json(updated))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+  });
+
 router.get('/', (req, res) => {
     return Journal.find()
         .then(journals => res.json(journals.map(journal => journal)))
@@ -45,7 +74,7 @@ router.get('/', (req, res) => {
 
 router.delete('/:id', (req, res) => {
     return Journal.findByIdAndRemove(req.params.id)
-        .then(() => res.status(204).send())
+        .then(() => res.status(204).end())
         .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
